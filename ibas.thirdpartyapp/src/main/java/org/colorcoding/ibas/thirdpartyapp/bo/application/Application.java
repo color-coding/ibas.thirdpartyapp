9241587336cb740abc392be487f1179a9ab73ad7
牛加人等于朱 <niuren.zhu@icloud.com>
2019-05-30 15:00:59 +0800

    使用BusinessObjectUnit替换BOCode

--- ibas.thirdpartyapp/src/main/java/org/colorcoding/ibas/thirdpartyapp/bo/application/Application.java
+++ ibas.thirdpartyapp/src/main/java/org/colorcoding/ibas/thirdpartyapp/bo/application/Application.java
@@ -12,7 +12,7 @@ import org.colorcoding.ibas.bobas.bo.IBOUserFields;
 import org.colorcoding.ibas.bobas.core.IPropertyInfo;
 import org.colorcoding.ibas.bobas.data.DateTime;
 import org.colorcoding.ibas.bobas.data.emYesNo;
-import org.colorcoding.ibas.bobas.mapping.BOCode;
+import org.colorcoding.ibas.bobas.mapping.BusinessObjectUnit;
 import org.colorcoding.ibas.bobas.mapping.DbField;
 import org.colorcoding.ibas.bobas.mapping.DbFieldType;
 import org.colorcoding.ibas.thirdpartyapp.MyConfiguration;
@@ -24,7 +24,7 @@ import org.colorcoding.ibas.thirdpartyapp.MyConfiguration;
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlType(name = Application.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
 @XmlRootElement(name = Application.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
-@BOCode(Application.BUSINESS_OBJECT_CODE)
+@BusinessObjectUnit(code = Application.BUSINESS_OBJECT_CODE)
 public class Application extends BusinessObject<Application> implements IApplication, IBOSeriesKey, IBOUserFields {
 
 	/**
--- ibas.thirdpartyapp/src/main/java/org/colorcoding/ibas/thirdpartyapp/bo/user/User.java
+++ ibas.thirdpartyapp/src/main/java/org/colorcoding/ibas/thirdpartyapp/bo/user/User.java
@@ -11,7 +11,7 @@ import org.colorcoding.ibas.bobas.bo.IBOUserFields;
 import org.colorcoding.ibas.bobas.core.IPropertyInfo;
 import org.colorcoding.ibas.bobas.data.DateTime;
 import org.colorcoding.ibas.bobas.data.emYesNo;
-import org.colorcoding.ibas.bobas.mapping.BOCode;
+import org.colorcoding.ibas.bobas.mapping.BusinessObjectUnit;
 import org.colorcoding.ibas.bobas.mapping.DbField;
 import org.colorcoding.ibas.bobas.mapping.DbFieldType;
 import org.colorcoding.ibas.thirdpartyapp.MyConfiguration;
@@ -23,7 +23,7 @@ import org.colorcoding.ibas.thirdpartyapp.MyConfiguration;
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlType(name = User.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
 @XmlRootElement(name = User.BUSINESS_OBJECT_NAME, namespace = MyConfiguration.NAMESPACE_BO)
-@BOCode(User.BUSINESS_OBJECT_CODE)
+@BusinessObjectUnit(code = User.BUSINESS_OBJECT_CODE)
 public class User extends BusinessObject<User> implements IUser, IBOUserFields {
 
 	/**
