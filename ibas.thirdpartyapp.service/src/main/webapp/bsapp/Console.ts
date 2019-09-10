color-coding/ibas.sales

升级代码，更新API

ibas.sales.service/src/main/webapp/bsapp/Console.ts
@@ -57,33 +57,36 @@ namespace sales {
             /** 运行 */
             run(): void {
                 // 加载语言-框架默认
-                ibas.i18n.load(this.rootUrl + "resources/languages/sales.json");
-                ibas.i18n.load(this.rootUrl + "resources/languages/bos.json");
-                // 设置资源属性
-                this.description = ibas.i18n.prop(this.name.toLowerCase());
-                this.icon = ibas.i18n.prop(this.name.toLowerCase() + "_icon");
-                // 先加载ui导航
-                let uiModules: string[] = [];
-                if (!ibas.config.get(ibas.CONFIG_ITEM_DISABLE_PLATFORM_VIEW, false)) {
-                    if (this.plantform === ibas.emPlantform.PHONE) {
-                        // 使用m类型视图
-                        uiModules.push("index.ui.m");
+                ibas.i18n.load([
+                    this.rootUrl + "resources/languages/sales.json",
+                    this.rootUrl + "resources/languages/bos.json"
+                ], () => {
+                    // 设置资源属性
+                    this.description = ibas.i18n.prop(this.name.toLowerCase());
+                    this.icon = ibas.i18n.prop(this.name.toLowerCase() + "_icon");
+                    // 先加载ui导航
+                    let uiModules: string[] = [];
+                    if (!ibas.config.get(ibas.CONFIG_ITEM_DISABLE_PLATFORM_VIEW, false)) {
+                        if (this.plantform === ibas.emPlantform.PHONE) {
+                            // 使用m类型视图
+                            uiModules.push("index.ui.m");
+                        }
                     }
-                }
-                // 默认使用视图
-                if (uiModules.length === 0) {
-                    // 使用c类型视图
-                    uiModules.push("index.ui.c");
-                }
-                // 加载视图库
-                this.loadUI(uiModules, (ui) => {
-                    // 设置导航
-                    this[PROPERTY_NAVIGATION] = new ui.Navigation();
-                    // 调用初始化
-                    this.initialize();
+                    // 默认使用视图
+                    if (uiModules.length === 0) {
+                        // 使用c类型视图
+                        uiModules.push("index.ui.c");
+                    }
+                    // 加载视图库
+                    this.loadUI(uiModules, (ui) => {
+                        // 设置导航
+                        this[PROPERTY_NAVIGATION] = new ui.Navigation();
+                        // 调用初始化
+                        this.initialize();
+                    });
+                    // 保留基类方法
+                    super.run();
                 });
-                // 保留基类方法
-                super.run();
             }
         }
         /** 模块控制台，手机端 */

color-coding/ibas.thirdpartyapp

ibas.thirdpartyapp.service/src/main/webapp/bsapp/Console.ts
@@ -41,33 +41,36 @@ namespace thirdpartyapp {
             /** 运行 */
             run(): void {
                 // 加载语言-框架默认
-                ibas.i18n.load(this.rootUrl + "resources/languages/thirdpartyapp.json");
-                ibas.i18n.load(this.rootUrl + "resources/languages/bos.json");
-                // 设置资源属性
-                this.description = ibas.i18n.prop(this.name.toLowerCase());
-                this.icon = ibas.i18n.prop(this.name.toLowerCase() + "_icon");
-                // 先加载ui导航
-                let uiModules: string[] = [];
-                if (!ibas.config.get(ibas.CONFIG_ITEM_DISABLE_PLATFORM_VIEW, false)) {
-                    if (this.plantform === ibas.emPlantform.PHONE) {
-                        // 使用m类型视图
-                        uiModules.push("index.ui.m");
+                ibas.i18n.load([
+                    this.rootUrl + "resources/languages/thirdpartyapp.json",
+                    this.rootUrl + "resources/languages/bos.json"
+                ], () => {
+                    // 设置资源属性
+                    this.description = ibas.i18n.prop(this.name.toLowerCase());
+                    this.icon = ibas.i18n.prop(this.name.toLowerCase() + "_icon");
+                    // 先加载ui导航
+                    let uiModules: string[] = [];
+                    if (!ibas.config.get(ibas.CONFIG_ITEM_DISABLE_PLATFORM_VIEW, false)) {
+                        if (this.plantform === ibas.emPlantform.PHONE) {
+                            // 使用m类型视图
+                            uiModules.push("index.ui.m");
+                        }
                     }
-                }
-                // 默认使用视图
-                if (uiModules.length === 0) {
-                    // 使用c类型视图
-                    uiModules.push("index.ui.c");
-                }
-                // 加载视图库
-                this.loadUI(uiModules, (ui) => {
-                    // 设置导航
-                    this[PROPERTY_NAVIGATION] = new ui.Navigation();
-                    // 调用初始化
-                    this.initialize();
+                    // 默认使用视图
+                    if (uiModules.length === 0) {
+                        // 使用c类型视图
+                        uiModules.push("index.ui.c");
+                    }
+                    // 加载视图库
+                    this.loadUI(uiModules, (ui) => {
+                        // 设置导航
+                        this[PROPERTY_NAVIGATION] = new ui.Navigation();
+                        // 调用初始化
+                        this.initialize();
+                    });
+                    // 保留基类方法
+                    super.run();
                 });
-                // 保留基类方法
-                super.run();
             }
         }
 
@@ -98,45 +101,48 @@ namespace thirdpartyapp {
             /** 运行 */
             run(): void {
                 // 加载语言-框架默认
-                ibas.i18n.load(this.rootUrl + "resources/languages/thirdpartyapp.json");
-                ibas.i18n.load(this.rootUrl + "resources/languages/bos.json");
-                // 设置资源属性
-                this.description = ibas.i18n.prop(this.name.toLowerCase());
-                this.icon = ibas.i18n.prop(this.name.toLowerCase() + "_icon");
-                // 先加载ui导航
-                let uiModules: string[] = [];
-                if (!ibas.config.get(ibas.CONFIG_ITEM_DISABLE_PLATFORM_VIEW, false)) {
-                    if (this.plantform === ibas.emPlantform.PHONE) {
-                        // 使用m类型视图
-                        uiModules.push("index.ui.m");
-                    }
-                }
-                // 默认使用视图
-                if (uiModules.length === 0) {
-                    // 使用c类型视图
-                    uiModules.push("index.ui.c");
-                }
-                // 加载视图库
-                this.loadUI(uiModules, (ui) => {
-                    // 设置导航
-                    this[PROPERTY_NAVIGATION] = new ui.Navigation();
-                    let boRepository: bo.BORepositoryThirdPartyApp = new bo.BORepositoryThirdPartyApp();
-                    boRepository.fetchUserApplications({
-                        user: ibas.variablesManager.getValue(ibas.VARIABLE_NAME_USER_CODE),
-                        onCompleted: (opRslt: ibas.IOperationResult<bo.UserApplication>) => {
-                            if (opRslt.resultCode !== 0) {
-                                ibas.logger.log(ibas.emMessageLevel.ERROR, opRslt.message);
-                            }
-                            for (let item of opRslt.resultObjects) {
-                                this.register(new UserApplicationFunc(item));
-                            }
-                            // 通知初始化完成
-                            this.fireInitialized();
+                ibas.i18n.load([
+                    this.rootUrl + "resources/languages/thirdpartyapp.json",
+                    this.rootUrl + "resources/languages/bos.json"
+                ], () => {
+                    // 设置资源属性
+                    this.description = ibas.i18n.prop(this.name.toLowerCase());
+                    this.icon = ibas.i18n.prop(this.name.toLowerCase() + "_icon");
+                    // 先加载ui导航
+                    let uiModules: string[] = [];
+                    if (!ibas.config.get(ibas.CONFIG_ITEM_DISABLE_PLATFORM_VIEW, false)) {
+                        if (this.plantform === ibas.emPlantform.PHONE) {
+                            // 使用m类型视图
+                            uiModules.push("index.ui.m");
                         }
+                    }
+                    // 默认使用视图
+                    if (uiModules.length === 0) {
+                        // 使用c类型视图
+                        uiModules.push("index.ui.c");
+                    }
+                    // 加载视图库
+                    this.loadUI(uiModules, (ui) => {
+                        // 设置导航
+                        this[PROPERTY_NAVIGATION] = new ui.Navigation();
+                        let boRepository: bo.BORepositoryThirdPartyApp = new bo.BORepositoryThirdPartyApp();
+                        boRepository.fetchUserApplications({
+                            user: ibas.variablesManager.getValue(ibas.VARIABLE_NAME_USER_CODE),
+                            onCompleted: (opRslt: ibas.IOperationResult<bo.UserApplication>) => {
+                                if (opRslt.resultCode !== 0) {
+                                    ibas.logger.log(ibas.emMessageLevel.ERROR, opRslt.message);
+                                }
+                                for (let item of opRslt.resultObjects) {
+                                    this.register(new UserApplicationFunc(item));
+                                }
+                                // 通知初始化完成
+                                this.fireInitialized();
+                            }
+                        });
                     });
+                    // 保留基类方法
+                    super.run();
                 });
-                // 保留基类方法
-                super.run();
             }
             /** 设置仓库地址 */
             setRepository(address: string): boolean {