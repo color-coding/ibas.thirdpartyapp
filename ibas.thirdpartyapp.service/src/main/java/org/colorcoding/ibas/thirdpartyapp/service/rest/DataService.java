color-coding/ibas.receiptpayment

支持请求头token

ibas.receiptpayment.service/src/main/java/org/colorcoding/ibas/receiptpayment/service/rest/DataService.java
@@ -1,6 +1,7 @@
 package org.colorcoding.ibas.receiptpayment.service.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 import org.colorcoding.ibas.bobas.common.Criteria;
 import org.colorcoding.ibas.bobas.common.OperationResult;
 import org.colorcoding.ibas.businesspartner.bo.internalreconciliation.InternalReconciliation;
+import org.colorcoding.ibas.receiptpayment.MyConfiguration;
 import org.colorcoding.ibas.receiptpayment.bo.assetrecharge.AssetRecharge;
 import org.colorcoding.ibas.receiptpayment.bo.payment.Payment;
 import org.colorcoding.ibas.receiptpayment.bo.receipt.Receipt;
@@ -33,8 +35,9 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchPayment")
-	public OperationResult<Payment> fetchPayment(Criteria criteria, @QueryParam("token") String token) {
-		return super.fetchPayment(criteria, token);
+	public OperationResult<Payment> fetchPayment(Criteria criteria, @HeaderParam("authorization") String authorization,
+			@QueryParam("token") String token) {
+		return super.fetchPayment(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
@@ -48,8 +51,9 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("savePayment")
-	public OperationResult<Payment> savePayment(Payment bo, @QueryParam("token") String token) {
-		return super.savePayment(bo, token);
+	public OperationResult<Payment> savePayment(Payment bo, @HeaderParam("authorization") String authorization,
+			@QueryParam("token") String token) {
+		return super.savePayment(bo, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//
@@ -64,8 +68,9 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchReceipt")
-	public OperationResult<Receipt> fetchReceipt(Criteria criteria, @QueryParam("token") String token) {
-		return super.fetchReceipt(criteria, token);
+	public OperationResult<Receipt> fetchReceipt(Criteria criteria, @HeaderParam("authorization") String authorization,
+			@QueryParam("token") String token) {
+		return super.fetchReceipt(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
@@ -79,8 +84,9 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("saveReceipt")
-	public OperationResult<Receipt> saveReceipt(Receipt bo, @QueryParam("token") String token) {
-		return super.saveReceipt(bo, token);
+	public OperationResult<Receipt> saveReceipt(Receipt bo, @HeaderParam("authorization") String authorization,
+			@QueryParam("token") String token) {
+		return super.saveReceipt(bo, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//
@@ -95,8 +101,9 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchAssetRecharge")
-	public OperationResult<AssetRecharge> fetchAssetRecharge(Criteria criteria, @QueryParam("token") String token) {
-		return super.fetchAssetRecharge(criteria, token);
+	public OperationResult<AssetRecharge> fetchAssetRecharge(Criteria criteria,
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.fetchAssetRecharge(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
@@ -110,8 +117,9 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("saveAssetRecharge")
-	public OperationResult<AssetRecharge> saveAssetRecharge(AssetRecharge bo, @QueryParam("token") String token) {
-		return super.saveAssetRecharge(bo, token);
+	public OperationResult<AssetRecharge> saveAssetRecharge(AssetRecharge bo,
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.saveAssetRecharge(bo, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//
@@ -127,8 +135,8 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchInternalReconciliation")
 	public OperationResult<InternalReconciliation> fetchInternalReconciliation(Criteria criteria,
-			@QueryParam("token") String token) {
-		return super.fetchInternalReconciliation(criteria, token);
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.fetchInternalReconciliation(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
@@ -143,8 +151,8 @@ public class DataService extends BORepositoryReceiptPayment {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("saveInternalReconciliation")
 	public OperationResult<InternalReconciliation> saveInternalReconciliation(InternalReconciliation bo,
-			@QueryParam("token") String token) {
-		return super.saveInternalReconciliation(bo, token);
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.saveInternalReconciliation(bo, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//

color-coding/ibas.thirdpartyapp

ibas.thirdpartyapp.service/src/main/java/org/colorcoding/ibas/thirdpartyapp/service/rest/DataService.java
@@ -1,6 +1,7 @@
 package org.colorcoding.ibas.thirdpartyapp.service.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,6 +10,7 @@ import javax.ws.rs.core.MediaType;
 
 import org.colorcoding.ibas.bobas.common.Criteria;
 import org.colorcoding.ibas.bobas.common.OperationResult;
+import org.colorcoding.ibas.thirdpartyapp.MyConfiguration;
 import org.colorcoding.ibas.thirdpartyapp.bo.application.Application;
 import org.colorcoding.ibas.thirdpartyapp.bo.applicationconfig.ApplicationConfig;
 import org.colorcoding.ibas.thirdpartyapp.bo.other.UserApplication;
@@ -33,8 +35,8 @@ public class DataService extends BORepositoryThirdPartyApp {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchUserApplications")
 	public OperationResult<UserApplication> fetchUserApplications(@QueryParam("user") String user,
-			@QueryParam("token") String token) {
-		return super.fetchUserApplications(user, token);
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.fetchUserApplications(user, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//
@@ -49,8 +51,9 @@ public class DataService extends BORepositoryThirdPartyApp {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchApplication")
-	public OperationResult<Application> fetchApplication(Criteria criteria, @QueryParam("token") String token) {
-		return super.fetchApplication(criteria, token);
+	public OperationResult<Application> fetchApplication(Criteria criteria,
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.fetchApplication(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
@@ -64,8 +67,9 @@ public class DataService extends BORepositoryThirdPartyApp {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("saveApplication")
-	public OperationResult<Application> saveApplication(Application bo, @QueryParam("token") String token) {
-		return super.saveApplication(bo, token);
+	public OperationResult<Application> saveApplication(Application bo,
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.saveApplication(bo, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//
@@ -81,8 +85,8 @@ public class DataService extends BORepositoryThirdPartyApp {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchApplicationConfig")
 	public OperationResult<ApplicationConfig> fetchApplicationConfig(Criteria criteria,
-			@QueryParam("token") String token) {
-		return super.fetchApplicationConfig(criteria, token);
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.fetchApplicationConfig(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	/**
@@ -97,8 +101,8 @@ public class DataService extends BORepositoryThirdPartyApp {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("saveApplicationConfig")
 	public OperationResult<ApplicationConfig> saveApplicationConfig(ApplicationConfig bo,
-			@QueryParam("token") String token) {
-		return super.saveApplicationConfig(bo, token);
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.saveApplicationConfig(bo, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//
@@ -113,8 +117,9 @@ public class DataService extends BORepositoryThirdPartyApp {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("fetchUserMapping")
-	public OperationResult<UserMapping> fetchUserMapping(Criteria criteria, @QueryParam("token") String token) {
-		return super.fetchUserMapping(criteria, token);
+	public OperationResult<UserMapping> fetchUserMapping(Criteria criteria,
+			@HeaderParam("authorization") String authorization, @QueryParam("token") String token) {
+		return super.fetchUserMapping(criteria, MyConfiguration.optToken(authorization, token));
 	}
 
 	// --------------------------------------------------------------------------------------------//