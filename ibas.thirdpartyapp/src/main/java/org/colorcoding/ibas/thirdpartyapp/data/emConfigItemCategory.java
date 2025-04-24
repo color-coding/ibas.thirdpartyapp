2ac00ed7564adfd4922e8493193e35a5eb757929
牛加人等于朱 <niuren.zhu@icloud.com>
2025-04-24 17:38:02 +0800

    v0.2.0

--- ibas.thirdpartyapp/src/main/java/org/colorcoding/ibas/thirdpartyapp/data/emConfigItemCategory.java
+++ ibas.thirdpartyapp/src/main/java/org/colorcoding/ibas/thirdpartyapp/data/emConfigItemCategory.java
@@ -2,7 +2,7 @@ package org.colorcoding.ibas.thirdpartyapp.data;
 
 import javax.xml.bind.annotation.XmlType;
 
-import org.colorcoding.ibas.bobas.mapping.Value;
+import org.colorcoding.ibas.bobas.common.Value;
 import org.colorcoding.ibas.thirdpartyapp.MyConfiguration;
 
 @XmlType(namespace = MyConfiguration.NAMESPACE_BO)
