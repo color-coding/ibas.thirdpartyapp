a60ddec4c07644c82ab917dfffeaeec0529f32c1
Niuren.Zhu <niuren.zhu@icloud.com>
2025-08-14 16:12:14 +0800

    优化代码

--- ibas.thirdpartyapp.service/src/main/webapp/bsapp/Console.ts
+++ ibas.thirdpartyapp.service/src/main/webapp/bsapp/Console.ts
@@ -45,9 +45,9 @@ namespace thirdpartyapp {
             /** 运行 */
             run(): void {
                 // 加载语言-框架默认
-                ibas.i18n.load([
-                    this.rootUrl + "resources/languages/thirdpartyapp.json",
-                    this.rootUrl + "resources/languages/bos.json"
+                this.loadResources([
+                    "resources/languages/thirdpartyapp.json",
+                    "resources/languages/bos.json"
                 ], () => {
                     // 设置资源属性
                     this.description = ibas.i18n.prop(this.name.toLowerCase());
@@ -105,9 +105,9 @@ namespace thirdpartyapp {
             /** 运行 */
             run(): void {
                 // 加载语言-框架默认
-                ibas.i18n.load([
-                    this.rootUrl + "resources/languages/thirdpartyapp.json",
-                    this.rootUrl + "resources/languages/bos.json"
+                this.loadResources([
+                    "resources/languages/thirdpartyapp.json",
+                    "resources/languages/bos.json"
                 ], () => {
                     // 设置资源属性
                     this.description = ibas.i18n.prop(this.name.toLowerCase());
